d6792ca567e9f56410b9a052cc2e46707cdd3f58
EvoNick-IR <mostafa.haghani69@gmail.com>
2023-11-10 18:22:46 +0330

    p1 j2 change to array

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-let counter = 0
-function btnSaz(){
+let btns =['.']
 
-    counter ++
-    const btn = document.createElement('div')
-    btn.className = 'btn'
-    btn.innerHTML = counter
-    btn.setAttribute('onclick','btnSaz()')
+function btnPush(){
+    btns.push('.')
+    btnSaz()
+}
     
-    document.body.append(btn)
+function btnSaz(){
+    document.body.innerHTML = ""
+    btns.forEach((btn,index)=>{
+        let counter = index +1
+        btn = document.createElement('div')
+        btn.className = 'btn'
+        btn.innerHTML = counter
+        btn.setAttribute('onclick','btnPush()')
+
+       
+        if(counter % 5 == 0){
+            btn.className = "btnHob"
+        }else if(counter % 2 == 0){
+            btn.style.backgroundColor = "#9b5de5"
+        }
+        document.body.append(btn)
+    })
 
 }
+
+let besaz = setInterval(btnPush,500)
+
+
+
+let flag = true
+document.addEventListener('keypress',(e)=>{
+
+    if(e.keyCode == 32 && flag){
+        clearInterval(besaz)
+        flag = false
+    }else {
+        besaz = setInterval(btnPush,500)
+        flag = true
+    }
+})
